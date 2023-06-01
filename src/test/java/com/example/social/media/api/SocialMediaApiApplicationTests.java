2a12b8a1c7c9b09318361074d2fd4c7766fe2c57
Daniil <rusbear060@gmail.com>
2023-06-01 17:14:52 +0300

    Initial commit

---
+++ src/test/java/com/example/social/media/api/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social.media.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
